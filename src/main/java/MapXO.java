9fa5292a8dc44a05f0f5182f4c548e6083918370
arste <mudaq@mail.ru>
2024-04-04 15:02:21 +0300

    fix: Перерисовал карту оптимизировал метод

--- src/main/java/MapXO.java
+++ src/main/java/MapXO.java
@@ -4,12 +4,30 @@ import java.util.Queue;
 public class MapXO {
     public void printMap(Figures[][] mapArray) {
 
+        Queue<String> queue = queueForMap(mapArray);
+
+        System.out.println();
+        System.out.println("= = = = =");
+        System.out.println();
+        printGameLine(queue);
+        System.out.println("- + - + -");
+        printGameLine(queue);
+        System.out.println("- + - + -");
+        printGameLine(queue);
+        System.out.println();
+        System.out.println("= = = = =");
+        System.out.println();
+    }
+
+    private static Queue<String> queueForMap(Figures[][] mapArray) {
         Queue<String> queue = new LinkedList<>();
+
+        String[][] plotArray = {{"1", "2", "3"},{"4", "5", "6"},{"7", "8", "9"}};
 
         for (int i = 0; i < mapArray[0].length; i++) {
             for (int j = 0; j < mapArray.length; j++) {
                 if (mapArray[i][j] == null) {
-                    queue.add(" ");
+                    queue.add(plotArray[i][j]);
                 } else if (mapArray[i][j] == Figures.CROSS) {
                     queue.add("X");
                 } else {
@@ -17,19 +35,7 @@ public class MapXO {
                 }
             }
         }
-
-
-        System.out.println();
-        System.out.println("= = = = =");
-        System.out.println();
-        printGameLine(queue);
-        System.out.println("- + - + -");
-        printGameLine(queue);
-        System.out.println("- + - + -");
-        printGameLine(queue);
-        System.out.println();
-        System.out.println("= = = = =");
-        System.out.println();
+        return queue;
     }
 
     private static void printGameLine(Queue<String> q) {
