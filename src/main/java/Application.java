40dae7ce22e05b9c095658bd40285e4bafdd36cf
arste <mudaq@mail.ru>
2025-01-15 12:28:20 +0300

    fix: Добавил выбор игроков

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,13 +3,14 @@ public class Application {
     /**
      * []Юнит тесты
      * [x]Перезапуск игры
-     * []Выбор одного игрока или 2х
+     * [x]Выбор одного игрока или 2х
      * []Узнать как делать консольное приложение
      * []Форсированный конец?
      */
 
     public static void main(String[] args) {
         boolean isExit;
+        boolean isFirst = false;
         do {
             GameLogic gameLogic = new GameLogic();
 
@@ -18,7 +19,7 @@ public class Application {
 
             //gameLogic.gameProcessSolo(newGame, currentGameMap);
 
-            gameLogic.gameProcessVsBot(newGame, currentGameMap, false);
+            gameLogic.gameProcessVsBot(newGame, currentGameMap, isFirst);
 
             isExit = gameLogic.isExit();
         } while (!isExit);
